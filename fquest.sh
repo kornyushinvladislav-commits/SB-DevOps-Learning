e4b1f179ff17587efcd68a074fa91767b2cd6c55
kornyushinvladislav-commits <kornyushin.vladislav@gmail.com>
2025-12-24 16:42:05 +0300

    Update fquest.sh
    Функционал создания и настройки сервера OpenVPN проверен полностью. Все функции работают

--- fquest.sh
+++ fquest.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
-
+#! /bin/bash 
+        
 echo "Hello, $USER!"
 
 SERVEROVPNDIR="/etc/openvpn/server/"
@@ -10,48 +10,21 @@ DATE_TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 echo "'$DATE_TIMESTAMP' : start ConfiguringFirewall ()" >> "$LOGFILENAME"
 sudo mkdir /home/$USER/log 2>> "$LOGFILENAME"
 
-#echo "S - server C - client"
-#read -p "Where will we work?  " where_wrk
-
 #============================================================================================================
 #============================================================================================================
-#if [ "$where_wrk== "S" ]; then
-#   echo "S will be selected"
-#else   
-#   echo "C will be selected"
-#fi
-        
 function testing_fnc ()
-{
+{       
    exit 0
-}       
+}
 #testing_fnc "enp0s3"
 #exit 1
 #============================================================================================================
 #============================================================================================================
-
-#echo -e "1 - Install SertCenter\n2 - Customize Sert. Center\n3 - Get server sertif\n4 - Server config-file\n\
-#5 - Start OpenVPN Server\n6 - Enable ip_forwarding\n7 - Configuring the firewall\n* - Exit"
-#read -p "Enter a number: " number
-
+        
 # Configuring the firewall
 # $1 - network interface
 function ConfiguringFirewall ()
-{
-        # use ip a (or ip -br a) for see network interface name.
-        # Result:
-        #   1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
-        #      link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
-        #      inet 127.0.0.1/8 scope host lo
-        #        valid_lft forever preferred_lft forever
-        #      inet6 ::1/128 scope host noprefixroute 
-        #        valid_lft forever preferred_lft forever
-        #   2: enp0s3: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
-        #      link/ether 08:00:27:e4:26:a3 brd ff:ff:ff:ff:ff:ff
-        #      inet 192.168.0.109/24 brd 192.168.0.255 scope global dynamic noprefixroute enp0s3
-        #        valid_lft 5933sec preferred_lft 5933sec
-        #      inet6 fe80::a00:27ff:fee4:26a3/64 scope link 
-        #        valid_lft forever preferred_lft forever
+{       
         echo "$DATE_TIMESTAMP : start ConfiguringFirewall ()" >> "$LOGFILENAME"
         sudo iptables -A INPUT -i $1 -m state --state NEW -p udp --dport 1194 -j ACCEPT 2>> "$LOGFILENAME"
         sudo iptables -A INPUT -i tun+ -j ACCEPT 2>> "$LOGFILENAME"
@@ -59,6 +32,9 @@ function ConfiguringFirewall ()
         sudo iptables -A FORWARD -i tun+ -o $1 -m state --state RELATED,ESTABLISHED -j ACCEPT 2>> "$LOGFILENAME"
         sudo iptables -A FORWARD -i $1 -o tun+ -m state --state RELATED,ESTABLISHED -j ACCEPT 2>> "$LOGFILENAME"
         sudo iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o $1 -j MASQUERADE 2>> "$LOGFILENAME"
+
+        # Need save this configuration
+        sudo netfilter-persistent save 2>> "$LOGFILENAME"
 }
 
 # Start OpenVPN Server service. After server.conf changes
@@ -77,6 +53,8 @@ function InstallOVPN_srv ()
         sudo apt-get update 2>> "$LOGFILENAME"
         # This is server. Install openvpn and easy-rsa
         sudo apt-get install openvpn easy-rsa 2>> "$LOGFILENAME"
+        # To store the configuration firewall wee need iptables-persistent       
+        sudo apt-get install iptables-persistent 2>> "$LOGFILENAME"
 }
 
 # Get server config-file
@@ -110,11 +88,9 @@ function GetServerConfigFile ()
                 "verb 3\n"\
                 "explicit-exit-notify 1" | sed 's/^[[:space:]]*//' >  "$SERVERCONFFILENAME"
 }
-
 # Customize Sert. Center
 function CustomSertCenter ()
 {
-        #date_timestamp=$(date +"%Y-%m-%d %H:%M:%S")
         echo "$DATE_TIMESTAMP : start CustomSertCenter () " >> "$LOGFILENAME"
         sudo mkdir /etc/openvpn/easy-rsa 2>> "$LOGFILENAME"
         sudo mkdir /etc/openvpn/easy-rsa/pki/ 2>> "$LOGFILENAME"
@@ -136,7 +112,6 @@ function CustomSertCenter ()
 # Get server sertificate
 function GetServerSertificate ()
 {
-        #date_timestamp=$(date +"%Y-%m-%d %H:%M:%S")
         echo "$DATE_TIMESTAMP : start GetServerSertificate () " >> "$LOGFILENAME"
         cd /etc/openvpn/easy-rsa/ 2>> "$LOGFILENAME"
 
@@ -151,9 +126,8 @@ function GetServerSertificate ()
         sudo cp ./pki/private/server.key /etc/openvpn/server/server.key 2>> "$LOGFILENAME"
 }
 
-echo -e "1 - Install SertCenter\n2 - Customize Sert. Center\n3 - Get server sertif\n4 - Server config-file\n\
-5 - Start OpenVPN Server\n6 - Enable ip_forwarding\n7 - Configuring the firewall\n* - Exit"
-read -p "Enter a number: " number
+echo -e '1 - Install SertCenter\n2 - Customize Sert. Center\n3 - Get server sertif\n4 - Server config-file\n5 - Start OpenVPN Server\n6 - Enable ip_forwarding\n7 - Configuring the firewall\n* - Exit'
+read -p 'Enter a number: ' number
 case $number in
         1)
                 echo 'pattern 1'
@@ -177,10 +151,17 @@ case $number in
                 ;;
         5)
                 echo 'pattern 5'
-                echo "Using /etc/openvpn/server/server.conf Server OVPN config-file"
+                echo 'Using /etc/openvpn/server/server.conf Server OVPN config-file'
                 # Start OpenVPN Server service. After server.conf changes.
+                pid_ovpn="$(pidof openvpn)"
+                if [ "$pid_ovpn" -gt 0 ]; then
+                        sudo kill -9 "$(pidof openvpn)" 2>> "$LOGFILENAME"
+                else
+                        echo 'Not found old openvpn process!'
+                fi
                 StartOVPN "$SERVERCONFFILENAME" 2>> "$LOGFILENAME"
-                echo "Do you see /Initialization Sequence Completed/?"
+                echo 'Do you see /Initialization Sequence Completed/?'
+                echo 'Don`t see this? Address already in use? Use this command: sudo kill -9 "$(pidof openvpn)"'
                 ;;
         6)
                 echo 'pattern 6'
@@ -191,6 +172,7 @@ case $number in
         7)
                 echo 'pattern 7'
                 #exit 1
+                # ip a (or ip -br a) for see network interface name.
                 ConfiguringFirewall enp0s3
                 ;;
         *)
@@ -199,3 +181,4 @@ case $number in
 esac
 
 echo 'The end of fquest!'
+
