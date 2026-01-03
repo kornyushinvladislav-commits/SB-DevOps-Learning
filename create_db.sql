48964ff7a8a992d06a3858a0f4db3574ca112f65
kornyushinvladislav-commits <kornyushin.vladislav@gmail.com>
2026-01-03 08:01:31 +0300

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS fquest_12;
+CREATE DATABASE IF NOT EXISTS fquest;
 
-USE fquest_12;
+USE fquest;
 
 CREATE TABLE ovpnclients (user_id INT AUTO_INCREMENT PRIMARY KEY, user_name VARCHAR(255), user_type INT);
 
@@ -9,3 +9,4 @@ CREATE TABLE ovpnfiles (file_id INT AUTO_INCREMENT PRIMARY KEY, file_name VARCHA
 CREATE TABLE IF NOT EXISTS server_files (sf_id INT NOT NULL, sfname VARCHAR(20) NOT NULL, sftext VARCHAR(20) NOT NULL);
 
 INSERT INTO ovpnclients (user_name, user_type) VALUES('Server', 'S');
+
